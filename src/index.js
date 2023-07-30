50ed8b16c4e03cd22c6ca4e3adf3be5b66f561dd
Callmesammy <callmesammy@yahoo.com>
2023-07-30 15:57:12 +0100

    zz

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import "swipper/css;"
+import "swipper/css";
 import "swipper/css/pagination";
 import "swipper/css/navigation";
 import "aos";
