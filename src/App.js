671f0ada76fad1b34d764d7f41e2342213fa9ea9
Callmesammy <callmesammy@yahoo.com>
2023-07-30 14:27:00 +0100

    ss

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 function App() {
-  return    <h1 className="text-3xl font-bold text-h1 underline"> Hello world!
+  return    <h1 className="text-3xl font-bold text-sub underline"> Hello world!
 </h1>;
 }
 export default App
