bdf8ab2f5dbd8698ba6ee0a1e6046f07a09b1206
Callmesammy <callmesammy@yahoo.com>
2023-07-30 17:55:04 +0100

    x

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import "swipper/css";
-import "swipper/css/pagination";
-import "swipper/css/navigation";
-import "aos";
-import "aos/dist/aos.css";
+import 'swiper/css';
+import 'swiper/css/navigation'; 
+import 'swiper/css/pagination';
+import 'aos';
+import 'aos/dist/aos.css';
 import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
