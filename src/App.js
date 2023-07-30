f27eff016759f68f29010cba85f767162f86805a
Callmesammy <callmesammy@yahoo.com>
2023-07-30 16:04:04 +0100

    xx

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 
 function App() {
-  return   <div className="h-header bg-maint w-full">
+  return   (<div className="h-header bg-maint w-full">
    <h1 className="text-h1 font-bold text-str underline"> Hello world! </h1>;
   </div>
+  );
 }
 export default App;
