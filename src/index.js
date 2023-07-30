5773795fdee9ee74bfe2f9076aceb0d949677533
Callmesammy <callmesammy@yahoo.com>
2023-07-30 15:53:59 +0100

    ss

--- src/index.js
+++ src/index.js
@@ -2,9 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import "swipper/css;"
-import "swipper/css/pagination";
-import "swipper/css/navigation";
 import "aos";
 import "aos/dist/aos.css";
 import { BrowserRouter } from 'react-router-dom';
