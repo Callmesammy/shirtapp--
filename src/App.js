e128408db8ab11011eea96ecf5c17fc578b379e9
Callmesammy <callmesammy@yahoo.com>
2023-08-03 00:21:18 +0100

    zz

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import Homescreen from './Screens/Dashboards/Homescreen';
+import Movies from './Screens/Dashboards/Movies';
+import Login from './Screens/Dashboards/Login';
+import Register from './Screens/Dashboards/Register';
+import NotFound from './Screens/NotFound';
 
 function App() {
   return (
-  <div className="h-header bg-maint w-full">
-   <h1 className="text-h1 font-bold text-str underline"> Hello wsorld! </h1>;
-    <p className="text-white">d d</p>
-  </div>
+    <Routes>
+      <Route path="/" element={<Homescreen/>} />
+      <Route path="/movies" element={<Movies/>} />
+      <Route path="/login" element={<Login/>} />
+      <Route path="/register" element={<Register/>} />
+      <Route path="*" element={<NotFound/>} />
+    </Routes>
   );
 }
 export default App;
---
+++ src/Screens/Dashboards/Contacus.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Contacus() {
+  return (
+    <div>Contacus</div>
+  )
+}
+
+export default Contacus
