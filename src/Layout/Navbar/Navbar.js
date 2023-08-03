6e2e40425bf26043647c87144ac783b04044449f
Callmesammy <callmesammy@yahoo.com>
2023-08-04 00:52:46 +0100

    ss

--- src/Layout/Navbar/Navbar.js
+++ src/Layout/Navbar/Navbar.js
@@ -1,9 +1,15 @@
 import React from 'react'
 
 function Navbar() {
-  return (
-    <div className=" bg-maint shadow-md sticky top-0 z-20">Navbar</div>
-  )
+  return 
+  <>
+    <div className=" bg-maint shadow-md sticky top-0 z-20">
+    <div className=" container mx-auto py-10 px-6 gap-12 lg:grid grid-cols-7 justify-between items-center">
+    <p> Navbar </p>
+    </div>
+    </div>
+
+    </>
 }
 
 export default Navbar
