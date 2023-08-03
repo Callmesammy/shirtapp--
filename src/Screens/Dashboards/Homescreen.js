63bdcb36fbcd0670127cf62c4d1212879d294836
Callmesammy <callmesammy@yahoo.com>
2023-08-04 00:29:27 +0100

    d

--- src/Screens/Dashboards/Homescreen.js
+++ src/Screens/Dashboards/Homescreen.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Homescreen() {
   return (
     <layout>
-       <div className="bg-maint h-2xl">Homescrfeen</div>
+       <div className="bg-maint h-2xl text-white">Homescrfeen</div>
     </layout>
    
   )
