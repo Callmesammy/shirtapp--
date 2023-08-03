fc43ea0516a35dc25997b1d2b929fc9405a7b39a
Callmesammy <callmesammy@yahoo.com>
2023-08-04 00:30:35 +0100

    s

--- src/Navbar/layout.js
+++ src/Navbar/layout.js
@@ -1,12 +1,13 @@
  import React from 'react'
   import Navbar from '../Navbar/navbar'
-      
+
  function layout({children}) {
    return (
     <>
          <div className="bg-maint text-white">
             <Navbar/>
             {children}
+            <footer/>
          </div>
     </>
    )
