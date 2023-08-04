67741556ce92f8a4dd2a2755428d0d3cb088afaa
Callmesammy <callmesammy@yahoo.com>
2023-08-04 02:00:13 +0100

    ss

--- src/Layout/Navbar/Navbar.js
+++ src/Layout/Navbar/Navbar.js
@@ -1,11 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Navbar() {
   return (
     <>
     <div className="bg-maint sticky shadow-md top-o z-20">
-    <div className="container mx-auto gap-10 b bg-sub py-12 px-6 lg:grid grid-cols-7 justify-center items-center">
-    <p> Navbar</p>
+    <div className="container mx-auto gap-10 py-12 px-6 lg:grid grid-cols-7 justify-center items-center">
+    <div className="col-span-7 lg:block hidden">
+        <Link to="/">
+        <img src="https://i.ibb.co/0jwZQpL/logo.jpg" alt="logo" 
+        className="w-full h-12 object-contain"/>
+        </Link>
     </div>
     </div>
     </>
