2c4d02264c62d8e66fd3f0b1904a40bea8f55db3
Callmesammy <callmesammy@yahoo.com>
2023-08-04 02:17:45 +0100

    rr

--- src/Layout/Navbar/Navbar.js
+++ src/Layout/Navbar/Navbar.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+
 function Navbar() {
   return (
     <>
@@ -8,7 +9,8 @@ function Navbar() {
     <div className="container mx-auto gap-10 py-12 px-6 lg:grid grid-cols-7 justify-center items-center">
     <div className="col-span-7 lg:block hidden">
         <Link to="/">
-        <img src="/pics/logo.jpg" alt="logo" 
+        <img 
+        src="/pics/logo.jpg" alt="logo" 
         className="w-full h-12 object-contain"/>
         </Link>
     </div>
