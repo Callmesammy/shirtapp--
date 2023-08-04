f85b29d9b0e0dc83b766724ac40cb61678114e24
Callmesammy <callmesammy@yahoo.com>
2023-08-04 02:42:11 +0100

    ss

--- src/Layout/Navbar/Navbar.js
+++ src/Layout/Navbar/Navbar.js
@@ -8,6 +8,7 @@ function Navbar() {
     <div className="bg-maint shadow-md top-0 z-10 sticky">
     <div className="container mx-auto gap-10 py-12 px-6 lg:grid grid-cols-7 justify-center items-center"> "
     </div>
+    </div>
     </>
   );
 }
