100e75317cc0d791b0bec0b7b05896821ae9dbf2
Callmesammy <callmesammy@yahoo.com>
2023-08-04 02:42:45 +0100

    s

--- src/Layout/Navbar/Navbar.js
+++ src/Layout/Navbar/Navbar.js
@@ -6,7 +6,7 @@ function Navbar() {
   return (
     <>
     <div className="bg-maint shadow-md top-0 z-10 sticky">
-    <div className="container mx-auto gap-10 py-12 px-6 lg:grid grid-cols-7 justify-center items-center"> 
+    <div className="container bg-sub mx-auto gap-10 py-12 px-6 lg:grid grid-cols-7 justify-center items-center"> 
     <p> Navbar</p>
     </div>
     </div>
