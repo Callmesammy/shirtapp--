ccea3f125567295cacc7caf28ad796d4a2118816
Callmesammy <callmesammy@yahoo.com>
2023-08-06 22:31:27 +0100

    ww

--- src/Layout/Navbar/Navbar.js
+++ src/Layout/Navbar/Navbar.js
@@ -2,7 +2,10 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <div className="bg-maint shadow-sm top-0">Navbar</div>
+    <div className="bg-maint shadow-sm top-0">
+      <div className="container mx-auto gap-10 px-2 lg:grid grid-cols-7 flex justify-between items-center">
+    </div>
+    </div>
   )
 }
 
