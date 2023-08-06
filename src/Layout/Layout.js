366c0fa9c8993ea6e65896ce53f72462596e3add
Callmesammy <callmesammy@yahoo.com>
2023-08-06 16:33:59 +0100

    ss

--- src/Layout/Layout.js
+++ src/Layout/Layout.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import Navbar from './Navbar/Navbar'
+import Footer from './Navbar/Footer'
 
 
-function Layout({}) {
-  return  <div>Layout</div>
-  
+function Layout({children}) {
+  return ( <>
+  <Navbar/>
+  <div className="bg-maint top-5">
+    {children}
+    <Footer/>
+  </div>
+  </>
+  )
 }
 
 export default Layout
