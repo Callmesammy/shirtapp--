e4160590423c5d02a6dd57eb40e2523aa19883cb
Callmesammy <callmesammy@yahoo.com>
2023-08-06 22:47:45 +0100

    ee

--- src/Layout/Navbar/Navbar.js
+++ src/Layout/Navbar/Navbar.js
@@ -2,9 +2,13 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <div className="bg-maint sticky shadow-sm top-0">
+    <div className="bg-maint sticky z-20 shadow-sm top-0">
       <div className="container mx-auto text-white gap-10 px-2 lg:grid grid-cols-7 flex justify-between items-center">
-      <p>hwshs</p>
+      <div className=" col-spaN-1 lg:block hidden">
+        <link to="/">
+          <img src="logo.jpg" alt="logo" className="w-20"/>
+        </link>
+    </div>
     </div>
     </div>
   )
