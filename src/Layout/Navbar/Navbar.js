363554d435a631ef07cada8c4460c9ecf5f36deb
Callmesammy <callmesammy@yahoo.com>
2023-08-06 22:32:08 +0100

    ss

--- src/Layout/Navbar/Navbar.js
+++ src/Layout/Navbar/Navbar.js
@@ -4,6 +4,7 @@ function Navbar() {
   return (
     <div className="bg-maint shadow-sm top-0">
       <div className="container mx-auto gap-10 px-2 lg:grid grid-cols-7 flex justify-between items-center">
+      <p>hwshs</p>
     </div>
     </div>
   )
