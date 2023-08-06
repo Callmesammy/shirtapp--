b0455c996dac36bccaccb04b7345b54119eb1efd
Callmesammy <callmesammy@yahoo.com>
2023-08-06 22:32:27 +0100

    dd

--- src/Layout/Navbar/Navbar.js
+++ src/Layout/Navbar/Navbar.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Navbar() {
   return (
     <div className="bg-maint shadow-sm top-0">
-      <div className="container mx-auto gap-10 px-2 lg:grid grid-cols-7 flex justify-between items-center">
+      <div className="container mx-auto text-white gap-10 px-2 lg:grid grid-cols-7 flex justify-between items-center">
       <p>hwshs</p>
     </div>
     </div>
