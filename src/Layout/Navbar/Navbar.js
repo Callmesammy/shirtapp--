47a0505dc8be5634d6b70eff66a8818c3b35b746
Callmesammy <callmesammy@yahoo.com>
2023-08-06 22:49:08 +0100

    ss

--- src/Layout/Navbar/Navbar.js
+++ src/Layout/Navbar/Navbar.js
@@ -7,9 +7,9 @@ function Navbar() {
     <div className="bg-maint sticky z-20 shadow-sm top-0">
       <div className="container mx-auto text-white gap-10 px-2 lg:grid grid-cols-7 flex justify-between items-center">
       <div className=" col-spaN-1 lg:block hidden">
-        <link to="/">
+        <Link to="/">
           <img src="logo.jpg" alt="logo" className="w-20"/>
-        </link>
+        </Link>
     </div>
     </div>
     </div>
