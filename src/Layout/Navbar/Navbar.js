6f9c31412ca9b92d4f2ab2b158360a96130733e7
Callmesammy <callmesammy@yahoo.com>
2023-08-09 00:11:02 +0100

    f

--- src/Layout/Navbar/Navbar.js
+++ src/Layout/Navbar/Navbar.js
@@ -15,7 +15,7 @@ function Navbar() {
     <div className="col-span-3">
       <form className="w-full text-sm bg-dyd rounded flex-btn gap-4">
         <button type="submit" className="bg-sub text-white flex-colo h-12 w-12 rounded-l">
-        <ImSearch className="w-10 h-10"/>
+        <ImSearch className="w-6 h-6"/>
         </button>
       </form>
     </div>
