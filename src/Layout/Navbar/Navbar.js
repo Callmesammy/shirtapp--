8f78d15f73ae014c0374f222f1db012792fe95c8
Callmesammy <callmesammy@yahoo.com>
2023-08-09 00:15:59 +0100

    ss

--- src/Layout/Navbar/Navbar.js
+++ src/Layout/Navbar/Navbar.js
@@ -18,7 +18,7 @@ function Navbar() {
         <ImSearch/>
         </button>
         <input type="text" placeholder="Search Movies here"
-         className="font-medium placeholder:text-txt w-11/12 h-12"/>
+         className="font-medium placeholder:text-brd w-11/12 h-12"/>
       </form>
     </div>
     </div>
