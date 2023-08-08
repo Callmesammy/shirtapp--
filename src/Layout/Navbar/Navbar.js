7770a294063f75fdfd281a9c1ca2a8680179b8d2
Callmesammy <callmesammy@yahoo.com>
2023-08-08 22:12:33 +0100

    ff

--- src/Layout/Navbar/Navbar.js
+++ src/Layout/Navbar/Navbar.js
@@ -5,7 +5,7 @@ import {Link} from 'react-router-dom'
 function Navbar() {
   return (
     <div className="bg-maint sticky z-20 shadow-sm top-0">
-      <div className="container mx-auto top-2 text-white gap-10 px-2 lg:grid grid-cols-7 flex justify-between items-center">
+      <div className="container mx-auto text-white gap-10 py-6 px-2 lg:grid grid-cols-7 flex justify-between items-center">
       <div className=" col-spaN-1 lg:block hidden">
         <Link to="/">
           <img src="/pics/logo.jpg" alt="logo" className="w-full object-contain"/>
