c4ef394bd63f72ad5a2766538a020db99f232dc9
Callmesammy <callmesammy@yahoo.com>
2023-08-11 23:26:19 +0100

    ss

--- src/Layout/Navbar/Navbar.js
+++ src/Layout/Navbar/Navbar.js
@@ -11,14 +11,16 @@ function Navbar() {
     <div className="z-20 top-0 sticky shadow-md bg-maint">
       <div className="container mx-auto px-6 gap-4 py-4 lg:grid grid-cols-7 justify-center items-center">
         <div className="col-span-1 lg:block hidden">
+        </div>
         <Link to="/">
           <img src="logo.png" alt="logo" className="w-full h-12 object-contain" />
+        
         </Link>
         <div className="col-span-3">
         <form className="w-full bg-txt rounded flex-colo h-12 ">
           <button type="submit" className="flex-btn bg-sub w-"></button>
           </form>
-          </div>
+          
       </div>
 
       
