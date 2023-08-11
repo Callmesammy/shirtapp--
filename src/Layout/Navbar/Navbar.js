82137225435fc9aade04976209a306bc0a46f221
Callmesammy <callmesammy@yahoo.com>
2023-08-11 23:09:35 +0100

    ff

--- src/Layout/Navbar/Navbar.js
+++ src/Layout/Navbar/Navbar.js
@@ -1,13 +1,14 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
-import {ImSearch} from 'react-icons/im'
+
 
 
 function Navbar() {
   return (
     <>
     <div className="z-20 top-0 sticky shadow-md text-white">
-      <div className="container mx-auto"
+      <div className="container mx-auto px-6 py-4 gap-10 lg:grid grid- cols-7 justify-center items-center">
+
+      </div>
     </div>
    </>
   );
