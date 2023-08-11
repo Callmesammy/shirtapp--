02c6e501bbb51bc09402401d9230e7fe5ce82215
Callmesammy <callmesammy@yahoo.com>
2023-08-11 23:15:18 +0100

    ss

--- src/Layout/Navbar/Navbar.js
+++ src/Layout/Navbar/Navbar.js
@@ -8,7 +8,7 @@ function Navbar() {
   return (
     <>
     <div className="z-20 top-0 sticky shadow-md bg-maint">
-      <div className="container mx-auto bg-sub px-6 py-4 gap-10 lg:grid grid- cols-7 justify-center items-center">
+      <div className="container mx-auto px-6 py-4 gap-10 lg:grid grid-cols-7 justify-center items-center">
         <Link to="/">
           <img src="logo.png" alt="logo" className="w-full h-12 object-contain" />
         </Link>
