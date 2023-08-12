5f365132e007a11922b72d025343c161b1f8bc56
Callmesammy <callmesammy@yahoo.com>
2023-08-12 08:18:21 +0100

    ss

--- src/Layout/Navbar/Navbar.js
+++ src/Layout/Navbar/Navbar.js
@@ -9,26 +9,8 @@ import { BiSearch } from 'react-icons/bi'
 function Navbar() {
   return (
     <>
-    <div className="z-20 top-0 sticky shadow-md bg-maint">
-      <div className="container mx-auto px-6 gap-4 py-4 lg:grid grid-cols-7 justify-center items-center">
-        <div className="col-span-1 lg:block hidden">
-        </div>
-        <Link to="/">
-          <img src="logo.png" alt="logo" className="w-full h-12 object-contain" />
-        
-        </Link>
-        <div className="col-span-3">
-        <form className="w-full bg-txt rounded flex-btn h-12 ">
-          <button type="submit" className="text-white bg-sub w-12 h-12 flex-colo rounded-l">
-            <BiSearch className="w-5 h-5"/>
-          </button>
-          </form>
-          
-      </div>
-
-      
-      </div>
-    </div>
+    <div className="bg-maint z-20"
+   
    </>
   );
 }
