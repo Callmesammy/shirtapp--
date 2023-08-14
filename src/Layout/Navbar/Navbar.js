73f22d3f46a2a156cfc886d8ee611f03eeed5102
Callmesammy <callmesammy@yahoo.com>
2023-08-14 18:14:16 +0100

    dd

--- src/Layout/Navbar/Navbar.js
+++ src/Layout/Navbar/Navbar.js
@@ -10,7 +10,7 @@ function Navbar() {
   return (
     <>
     <div className="bg-maint z-20 top-0 sticky shadow-sm">
-      <div className="container mx-auto px-4 py-6 gap-4 text-white">
+      <div className="container grid-cols-7 lg:grid bg-sub mx-auto px-4 py-6 gap-4 items-center justify-center text-white">
       <div className=" col-span-1 lg:block hidden">
       
    </div>
