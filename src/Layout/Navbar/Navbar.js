e0f3d1634be738cd07ebf0903a788951b4b7236b
Callmesammy <callmesammy@yahoo.com>
2023-08-15 20:34:55 +0100

    ss

--- src/Layout/Navbar/Navbar.js
+++ src/Layout/Navbar/Navbar.js
@@ -20,7 +20,7 @@ function Navbar() {
       <BiSearchAlt className="w-5 h-5"/>
     </button>
     <input type="text" placeholder="Search your movies hear and download for free" 
-    className="placeholder:boder text-white bg-white w-11/12 h-12 bg-transparent rounded border">
+    className="placeholder:border text-white bg-white w-11/12 h-12 bg-transparent rounded border">
       
     </input>
     </div>
