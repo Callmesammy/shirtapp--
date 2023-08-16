1ac8a6ffe137b2ccfac1836ab2e4b2be6acd5381
Callmesammy <callmesammy@yahoo.com>
2023-08-16 03:10:58 +0100

    c

--- src/Layout/Navbar/Navbar.js
+++ src/Layout/Navbar/Navbar.js
@@ -40,7 +40,7 @@ function Navbar() {
       <NavLink to="/about" className={Sober}>About</NavLink>
       <NavLink to="/login" className={`${Sober}relative`}><AiFillHeart className="w-6 h-6"
       />
-     <div className="w-5 h-5 flex-colo rounded-full bg-sub text-white text-xs absolute -top-1 -right-2">
+     <div className="w-5 h-5 flex-colo rounded-full bg-sub text-white text-xs absolute -top+3 -right-2">
       4
      </div>
       </NavLink>
