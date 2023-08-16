18013cec8f96e331a85968a25650610bca396c55
Callmesammy <callmesammy@yahoo.com>
2023-08-16 02:06:21 +0100

    ss

--- src/Layout/Navbar/Navbar.js
+++ src/Layout/Navbar/Navbar.js
@@ -33,6 +33,9 @@ function Navbar() {
     {/*Menu*/}
     <div className="col-span-3 font-normal text-sm justify-between xl:gap-14 2xl:gap-20 lg:flex hidden xl:justify-end items-center">
       <NavLink to="/movies" className={Sober}>Movies</NavLink>
+      <NavLink to="/tvshows" className={Sober}>TV Shows</NavLink>
+      <NavLink to="/upcoming" className={Sober}>Upcoming</NavLink>
+      <NavLink to="/contact" className={Sober}>Contact</NavLink>
     </div>
     </div>
     
