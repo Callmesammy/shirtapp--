e00a62075b9993907aa68c1f665cdb1a8c8761c3
Callmesammy <callmesammy@yahoo.com>
2023-08-16 01:23:06 +0100

    ss

--- src/Layout/Navbar/Navbar.js
+++ src/Layout/Navbar/Navbar.js
@@ -7,13 +7,16 @@ import { BiSearchAlt } from "react-icons/bi";
 function Navbar() {
   return (
     <>
+    {/*Navbar*/}
     <div className="bg-maint z-20 top-0 sticky shadow-sm">
       <div className="container grid-cols-7 lg:grid mx-auto px-4 py-6 gap-4 items-center justify-center text-white">
+         {/*Logo*/}
       <div className=" col-span-1 lg:block hidden">
       <Link to="/">
       <img src="logo.png" alt="logo" className="w-full h-12 object-contain " />
       </Link>
    </div>
+       {/*Search bar*/}
     <div className="col-span-3 bg-maint ">
     <div className="flex  bg-white rounded text-sm gap-4 text-white flex-btn w-full h-12 items-center justify-center">
     <button type="submit" className="bg-sub w-12 h-12 text-white rounded-l flex-colo">
@@ -21,11 +24,16 @@ function Navbar() {
     </button>
     <input type="text" placeholder="Search your movies hear and download for free" 
     className="placeholder:text-border font-medium border-none text-sm px-4 text-black bg-white w-11/12 h-12 bg-transparent rounded ">
-      
+
     </input>
     </div>
     </div>
-   </div>
+    {/*Menu*/}
+    <div className="col-span-3 font-normal text-sm justify-between xl:gap-14 2xl:gap-20 lg:flex hidden xl:justify-end items-center">
+      <Link to="/movies" className="hover:text-sub">Movies</Link>
+    </div>
+    </div>
+    
     </div>
    </>
   );
