b4c5f49a2ffe99324547f42e912700044d77e3f9
Callmesammy <callmesammy@yahoo.com>
2023-08-18 02:16:25 +0100

    asa

--- src/Layout/Navbar/Footer.js
+++ src/Layout/Navbar/Footer.js
@@ -78,7 +78,7 @@ function Footer() {
       <div key={index} className="col-span-1 md:col-span-2 lg:col-span-3 pb-3.5 sm:pb-0">
       <h3 className="text-white font-bold text-md lg:leading-7 mb-4 lg:mb-6 pb-3.5">{links.title}</h3>
       <ul className="text-s,  flex flex-col space-y-3">
-        {Link.links.map((text, index) => (
+        {links.links.map((text, index) => (
           <li key={index} className="flex items-baseline">
             <Link to={text.link} className="text-white hover:text-sub transition">
               {text.name}</Link>
