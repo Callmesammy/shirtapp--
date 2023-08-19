fc06e2075ce8651e5080ceb0e4c4bbc82f18d92a
Callmesammy <callmesammy@yahoo.com>
2023-08-19 23:24:32 +0100

    ss

--- src/Layout/Navbar/Footer.js
+++ src/Layout/Navbar/Footer.js
@@ -2,8 +2,13 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <div className="bg-dyr2 border-t-2 border">
+    <div className="container mx-auto px-2">
+    <div className="grid-cols-3 grid sm:grid-cols-7 md:grid-cols-9 lg:grid-cols-12 xl:grid-cols-14 gap sm:gap-2 md:gap-6 lg:gap-8 xl:gap-10 py-6 justify-between">
+      </div>
+      </div>
+      </div>
+  );
 }
 
 export default Footer
