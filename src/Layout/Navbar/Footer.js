7197ddcf8194075260768aedd05482f909a3f900
Callmesammy <callmesammy@yahoo.com>
2023-08-20 00:56:31 +0100

    ss

--- src/Layout/Navbar/Footer.js
+++ src/Layout/Navbar/Footer.js
@@ -68,7 +68,7 @@ function Footer() {
   ]
   return (
     <div className="bg-dyr border-t-2 border-black">
-    <div className=" container mx-auto px-2">
+    <div className=" container mx-auto px-4">
     <div className="grid-cols-1 sm:grid-cols-3 md:grid-cols-7 lg:grid-cols-9 xl:grid-cols-7 gap-5 sm:gap-7 md:gap-9 lg:gap-10 xl:gap-12 py-6 justify-between">
     {Footer.map((item, index) => (
     <div key={index} className="col-span-1 sm:col-span-2 md:col-span-3 lg:col-span-4">
