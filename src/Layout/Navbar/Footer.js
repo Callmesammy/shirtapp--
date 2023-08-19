6a791a60c456f50f26376908531b462037f791ef
Callmesammy <callmesammy@yahoo.com>
2023-08-20 00:52:41 +0100

    dd

--- src/Layout/Navbar/Footer.js
+++ src/Layout/Navbar/Footer.js
@@ -45,7 +45,7 @@ function Footer() {
       ]
     },
      {
-      title: "Categorisd", 
+      title: "Categories", 
       links:[
         {
           name: "Action",
@@ -71,7 +71,7 @@ function Footer() {
     <div className=" container mx-auto px-2">
     <div className="grid-cols-1 sm:grid-cols-3 md:grid-cols-7 lg:grid-cols-9 xl:grid-cols-7 gap-5 sm:gap-7 md:gap-9 lg:gap-10 xl:gap-12 py-6 justify-between">
     {Footer.map((item, index) => (
-    <div key={index} className="col-span-1 sm:col-span-2 md:col-span-3 lg:col-span-4">
+    <div key={index} className="col-span-1 sm:col-span-2 md:col-span-3 lg:col-span-4 justify-between">
       <h3 className="lg:leading-7 text-white text-md font-normal">{item.title}</h3>
    </div> ))}
       </div>
