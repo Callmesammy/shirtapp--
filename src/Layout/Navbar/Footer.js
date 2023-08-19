93d1a70bba456249e89362563e80e3d07703650c
Callmesammy <callmesammy@yahoo.com>
2023-08-20 00:01:57 +0100

    dd

--- src/Layout/Navbar/Footer.js
+++ src/Layout/Navbar/Footer.js
@@ -1,11 +1,86 @@
 import React from 'react'
 
 function Footer() {
+  const Footers = [
+    {
+    title: "HomePage",
+    links:[
+      {
+         name: "About-us",
+         link: "/about-us"
+      },
+      {
+        name: "Contact-us",
+        link: "/contact-us"
+     },
+     {
+      name: "Reach-us",
+      link: "/reach-us"
+   },
+   {
+    name: "Email-us",
+    link: "/email-us"
+   },
+    ]
+
+    },
+    {
+      title: "Company",
+      links:[
+        {
+           name: "Movies",
+           link: "#"
+        },
+        {
+          name: "Login",
+          link: "#"
+       },
+       {
+        name: "Contact",
+        link: "#"
+     },
+     {
+      name: "About",
+      link: "#"
+     },
+      ]
+  
+      },
+      {
+        title: "Categories",
+        links:[
+          {
+             name: "Action",
+             link: "/action"
+          },
+          {
+            name: "Adventure",
+            link: "/adventure"
+         },
+         {
+          name: "Fantancy",
+          link: "/fantancy"
+       },
+       {
+        name: "Comedy",
+        link: "/comedy"
+       },
+        ]
+    
+        },
+  
+];
   return (
     <div className="bg-dyr border-t-2 border-black">
     <div className="container mx-auto px-2">
     <div className="grid-cols-3 grid sm:grid-cols-7 md:grid-cols-9 lg:grid-cols-12 xl:grid-cols-14 gap sm:gap-2 md:gap-6 lg:gap-8 xl:gap-10 py-6 justify-between">
-    ghsdgh </div>
+    {Footers.map((footer, index) => (
+      <div key={index}>
+    <h3 className="text-white text-sm col-span-7 font-normal">
+      {footer.title}</h3>
+      </div>
+    ))}
+    </div>
       </div>
       </div>
   );
