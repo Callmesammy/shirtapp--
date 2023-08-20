d650e4a7b2570067c828a7392703c843def68e45
Callmesammy <callmesammy@yahoo.com>
2023-08-20 13:04:36 +0100

    ss

--- src/Layout/Navbar/Footer.js
+++ src/Layout/Navbar/Footer.js
@@ -5,6 +5,7 @@ function Footer() {
     <div  className="bg-dyr border-t-3 border-black">
     <div className="container mx-auto px-2">
     <div className="grid sm:grid-cols-3 py-6 md:grid-cols-5 lg:grid-cols-7 xl:grid-cols-12 gap-5 md:gap-7 sm:gap-3 lg:gap-9 xl:gap-12 pb-3.5 sm:pb-0">
+    sasa
     
     </div>  ss
     </div>
