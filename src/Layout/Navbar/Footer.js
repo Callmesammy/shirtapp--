cd78fe30d5df99fcd89f229fdf079adcfa186da6
Callmesammy <callmesammy@yahoo.com>
2023-08-20 03:24:56 +0100

    ff

--- src/Layout/Navbar/Footer.js
+++ src/Layout/Navbar/Footer.js
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <div className="bg-dyr border-t-3 border-black">
     <div className="container mx-20 gap-2">
-    <div className="gap-5 grid justify-between py-6 mb-6 sm:gap-3 md:gap-5 lg:gap-8 xl:gap-12 sm:grid-cols-2 md:grid-cols-5 lg:grid-cols-7 xl:grid-cols-12">
+    <div className="gap-5 grid justify-between py-8 sm:gap-3 md:gap-5 lg:gap-8 xl:gap-12 sm:grid-cols-2 md:grid-cols-5 lg:grid-cols-7 xl:grid-cols-12">
     Footer
       </div>
       </div>
