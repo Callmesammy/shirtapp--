e772dd529b4dbc69c30e023f5b11552d0e2b1aff
Callmesammy <callmesammy@yahoo.com>
2023-08-20 03:34:34 +0100

    dd

--- src/Layout/Navbar/Footer.js
+++ src/Layout/Navbar/Footer.js
@@ -1,11 +1,83 @@
 import React from 'react'
 
 function Footer() {
+  const Links =[
+    {
+      title: "HomePage",
+      link:[
+        {
+          name: "Home",
+          link: "/"
+        },
+        {
+          name: "AboutUs",
+          link: "/about-us"
+        },
+        {
+          name: "ContactUs",
+          link: "/contact-us"
+        },
+        {
+          name: "EmailUs",
+          link: "/email-us"
+        },
+      ]
+
+    },
+    {
+      title: "Categories",
+      link:[
+        {
+          name: "Actions",
+          link: "#"
+        },
+        {
+          name: "Adventure",
+          link: "#"
+        },
+        {
+          name: "Fantasy",
+          link: "#"
+        },
+        {
+          name: "Romance",
+          link: "#"
+        },
+      ]
+
+    },
+    {
+      title: "Movies",
+      link:[
+        {
+          name: "Iced",
+          link: "/ice"
+        },
+        {
+          name: "Filmy",
+          link: "/filmy"
+        },
+        {
+          name: "Dabra",
+          link: "/dab"
+        },
+        {
+          name: "Sin",
+          link: "/sin"
+        },
+      ]
+
+    },
+    
+]
   return (
     <div className="bg-dyr border-t-3 border-black">
     <div className="container mx-20 gap-2">
     <div className="gap-5 grid justify-between py-8 sm:gap-3 md:gap-5 lg:gap-8 xl:gap-12 sm:grid-cols-2 md:grid-cols-5 lg:grid-cols-7 xl:grid-cols-12">
-    Footer
+        {Links.map((link, index) => (
+        <div key={index} className="col-span-1 sm:col-span-2 md:col-span-3 lg:col-span-4 xl:col-span-5 justify-between pb-3.5 sm:pb-0">
+          <h3 className="font-bold text-white">{link.title}</h3>=
+        </div>))}
       </div>
       </div>
       </div>
