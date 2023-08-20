450e783829cfb44c366292c40fcad45bb261c157
Callmesammy <callmesammy@yahoo.com>
2023-08-20 05:10:24 +0100

    ss

--- src/Layout/Navbar/Footer.js
+++ src/Layout/Navbar/Footer.js
@@ -1,11 +1,10 @@
 import React from 'react'
 
-
 function Footer() {
   const Links =[
     {
       title: "HomePage",
-      links:[
+      link:[
         {
           name: "Home",
           link: "/"
@@ -27,7 +26,7 @@ function Footer() {
     },
     {
       title: "Categories",
-      links:[
+      link:[
         {
           name: "Actions",
           link: "#"
@@ -49,7 +48,7 @@ function Footer() {
     },
     {
       title: "Movies",
-      links:[
+      link:[
         {
           name: "Iced",
           link: "/ice"
@@ -78,7 +77,13 @@ function Footer() {
         {Links.map((link, index) => (
         <div key={index} className="col-span-1 sm:col-span-2 md:col-span-3 lg:col-span-2 xl:col-span-3 justify-between pb-3.5 sm:pb-0">
           <h3 className="font-bold text-white">{link.title}</h3>
-          
+          <ul className="flex flex-col space-y-3">
+            {link.link.map((item, index) => (
+              <li key={index} className="justify-between mb-5 sm:mb-4 lg:mb-6 xl:mb-7 pb-0.5">
+              <link to ={item.link} className="text-brd hover:text-sub">{item.name}</link>
+              </li>
+            ))}
+          </ul>
         </div>))}
       </div>
       </div>
