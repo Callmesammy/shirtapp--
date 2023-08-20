1b2af6d35184705c84294ea63d4540fd501859a9
Callmesammy <callmesammy@yahoo.com>
2023-08-20 02:42:14 +0100

    ss

--- src/Layout/Navbar/Footer.js
+++ src/Layout/Navbar/Footer.js
@@ -70,11 +70,11 @@ function Footer() {
     <div className="bg-dyr border-t-2 border-black">
     <div className=" container mx-auto px-2">
     <div className="grid-cols-1 grid sm:grid-cols-2 md:grid-cols-5 lg:grid-cols-9 xl:grid-cols-12 gap-5 sm:gap-7 md:gap-9 lg:gap-10 xl:gap-12 py-6 justify-between">
-      {Footer.map((link, index) =>(
+      {Footer.map((text, index) =>(
         <div key={index} className="col-span-1 md-col-span-2 lg:col-span-3 xl:col-span-4 pb-3.5 sm:pb-0">
-        <h3 className="lg:leading-7 text-bold font-bold text-white mb-4 sm:mb-2">{link.title}</h3>  
+        <h3 className="lg:leading-7 text-bold font-bold text-white mb-4 sm:mb-2">{text.title}</h3>  
         <ul className="flex flex-col space-y-3">
-          {link.links.map((link, index) =>(
+          {text.links.map((link, index) =>(
             <li key={index} className="flex items-baseline">
         <links to={link.url} className="text-white text-sm font-medium hover:text-gray-300">{link.name}</links>
         </li>
