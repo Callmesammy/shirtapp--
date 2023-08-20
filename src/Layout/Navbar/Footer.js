fb13917316de673230065ffe1ffe2c6f50b23c4c
Callmesammy <callmesammy@yahoo.com>
2023-08-20 16:24:38 +0100

    ss

--- src/Layout/Navbar/Footer.js
+++ src/Layout/Navbar/Footer.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
 
 function Footer() {
   const Links =[
@@ -71,24 +70,8 @@ function Footer() {
   return (
     <div  className="bg-dyr border-t-3 border-black">
     <div className="container mx-auto px-2">
-    <div className="grid sm:grid-cols-2 py-6 md:grid-cols-3 lg:grid-cols-7 xl:grid-cols-10 gap-5 mb-5 md:gap-5 sm:gap-4 lg:gap-6 xl:gap-10 pb-3.5 sm:pb-0">
-    {Links.map((link, index) => (
-          <div key={index} className="col-span-1 md:col-span-2 xl:col-3 mb-5 sm:mb-3 pb-0.5">
-      < h3 className="font-bold mb-4 text-white">{link.title}</h3>
-      <ul className="flex text-sm space-y-3 flex-col">
-        {link.links.map((text, index) => (
-          <li key={index} className="flex items-baseline">
-          <Link to={text.link}
-           className="inline-block hover:text-sub w-full text-brd"> 
-          {text.name}
-          </Link>
-          </li>
-        ))}
-      </ul>
-      </div>
-
-    ))}
-
+    <div className="grid sm:grid-cols-3 py-6 md:grid-cols-5 lg:grid-cols-7 xl:grid-cols-12 gap-5 md:gap-7 sm:gap-3 lg:gap-9 xl:gap-12 pb-3.5 sm:pb-0">
+    
 
     </div>
     </div>
