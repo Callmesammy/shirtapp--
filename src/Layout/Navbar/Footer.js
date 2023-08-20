4ee5efe4970ee7f5476ab1cc664fdca4bf9649ab
Callmesammy <callmesammy@yahoo.com>
2023-08-20 05:06:44 +0100

    x

--- src/Layout/Navbar/Footer.js
+++ src/Layout/Navbar/Footer.js
@@ -1,5 +1,6 @@
 import React from 'react'
 
+
 function Footer() {
   const Links =[
     {
@@ -78,7 +79,7 @@ function Footer() {
         <div key={index} className="col-span-1 sm:col-span-2 md:col-span-3 lg:col-span-2 xl:col-span-3 justify-between pb-3.5 sm:pb-0">
           <h3 className="font-bold text-white">{link.title}</h3>
           <ul className="flex flex-col space-y-3">
-            {linkz.links.map((link, index) => (
+            {link.links.map((link, index) => (
               <li key={index} className="justify-between mb-3 sm:mb-4 lg:mb-5 xl:mb-6 pb-0.5">
               <Links to={link.link} className="text-white hover:text-yellow-500">{link.name}</Links>
               </li>
