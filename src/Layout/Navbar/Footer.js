7c3f20abe7ed7db6a5d768144419b9995dbf5583
Callmesammy <callmesammy@yahoo.com>
2023-08-22 00:20:23 +0100

    s

--- src/Layout/Navbar/Footer.js
+++ src/Layout/Navbar/Footer.js
@@ -72,11 +72,15 @@ function Footer() {
     <div className="container mx-auto px-4">  
     <div className="grid sm:grid-cols-3 md:grid-cols-7 xl:grid-cols-12 gap-5 sm:gap-6 md:gap-7 xl:gap-14 py-5 justify-between pb-3.5 sm:pb-0">
     {Links.map((link, index) => (
-      <div key={index} className="col-span-1 sm:col-span-3 md:col-span-5 xl:col-span-8">
-      <h1 className="text-white font-medium text-sm mb-3">{link.title}</h1>
+      <div key={index} className="col-span-1 sm:col-span-3 md:col-span-4 xl:col-span-4 mb-4 sm:mb-0 pb-0.5">
+      <h3 className="font-medium text-sm text-white mb-4 sm:mb-0 pb-0.5">
+      {link.title}
+      </h3>
+      </div>
+    ))}
     </div>
     
-     
+     </div>
     </div>
     </div>
   )
