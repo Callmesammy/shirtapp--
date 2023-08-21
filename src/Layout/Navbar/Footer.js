7b7563e00c31337b386c8e54f63e7122b08b8305
Callmesammy <callmesammy@yahoo.com>
2023-08-22 00:51:43 +0100

    dd

--- src/Layout/Navbar/Footer.js
+++ src/Layout/Navbar/Footer.js
@@ -77,10 +77,10 @@ function Footer() {
       <h3 className=" text-white font-bold space-y-3 mb-4 ">
       {link.title}
       </h3>
-      <ul className="flex flex-col items-baseline space-y-3">
+      <ul className="flex flex-col space-y-3">
       {link.links.map((item, index) => (
-        <li key={index} className="flex justify-between">
-        <Link to={item.link} className="text-white hover:text-sub">
+        <li key={index} className="flex items-baseline  justify-between">
+        <Link to={item.link} className="text-white w-full hover:text-sub">
          </Link>
         </li>
       ))}
