9a4074c51ec631db99178a9c8ef0c683c8840dc2
Callmesammy <callmesammy@yahoo.com>
2023-08-21 23:39:50 +0100

    ss

--- src/Layout/Navbar/Footer.js
+++ src/Layout/Navbar/Footer.js
@@ -5,7 +5,7 @@ function Footer() {
     <div className="bg-dyr border-t-3 border-black">
     <div className="container mx-auto px-4">  
     <div className="grid sm:grid-cols-3 md:grid-cols-7 xl:grid-cols-12 gap-5 sm:gap-6 md:gap-7 xl:gap-14 py-5 justify-between pb-3.5 sm:pb-0">
-    
+    ss
     </div>
     
      
