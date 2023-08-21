492a709973bddb1ecc2721a1c29bb9a9399b5ea7
Callmesammy <callmesammy@yahoo.com>
2023-08-21 23:34:46 +0100

    ss

--- src/Layout/Navbar/Footer.js
+++ src/Layout/Navbar/Footer.js
@@ -2,7 +2,12 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <div className="bg-dyr border-t-3 border-black">
+    <div className="container mx-auto px-4">
+    
+     
+    </div>
+    </div>
   )
 }
 
