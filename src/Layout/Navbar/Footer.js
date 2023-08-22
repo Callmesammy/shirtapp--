47b9c8a62dde157b88021459bac389ab4cfa8493
Callmesammy <callmesammy@yahoo.com>
2023-08-22 01:02:15 +0100

    dd

--- src/Layout/Navbar/Footer.js
+++ src/Layout/Navbar/Footer.js
@@ -69,7 +69,7 @@ function Footer() {
     },
   ];
   return (
-    <div className="bg-dyr border-t-2 border-black">
+    <div className="bg-dyr border-t-2 py-4 border-black">
     <div className="container mx-auto px-2">  
     <div className="grid sm:grid-cols-3 md:grid-cols-7 xl:grid-cols-12 gap-5 sm:gap-6 md:gap-7 xl:gap-14 py-4 justify-between pb-3.5 sm:pb-0">
     {Links.map((link, index) => (
