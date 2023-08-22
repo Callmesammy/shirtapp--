978e7c0a86cbfbab1a7a279fc0e56032ed417738
Callmesammy <callmesammy@yahoo.com>
2023-08-23 00:35:48 +0100

    d

--- src/Layout/Navbar/Footer.js
+++ src/Layout/Navbar/Footer.js
@@ -92,8 +92,9 @@ function Footer() {
       <Link to="/">
       <img src="logo.png" alt="logo" className="w-2/4 h-12 object-contain" />
       </Link>
+           <p className="text-sm lg:leading-7 text-brd ">
+
       <span>
-     <p className="text-sm lg:leading-7 text-brd ">
       14B Grace Angous drive,   <br/> Lekki Phase 1, Lagos
       </span>
       <br/>
@@ -103,7 +104,8 @@ function Footer() {
       <br/>
       <span>
         Email: callmesammy@yahoo.com
-      </span></p>
+      </span>
+      </p>
     </div>
     </div>
     
