b80fa33e1f5ba0c7a4649d41ffcff4ff2e36444d
Callmesammy <callmesammy@yahoo.com>
2023-08-29 00:03:44 +0100

    ss

--- src/Home/Banner.js
+++ src/Home/Banner.js
@@ -1,14 +1,22 @@
 import React from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react';
 
-
 function Banner() {
   return (
     <div className="relative w-full">
     <Swiper 
- 
+    direction="vertical"
+    spaceBetween={0}
+    slidersPerView={1}
+    loop={true}
+    speed={1000}
+    modules={[Autoplay]}
+    autoplay={{delay:4000, disableOnInteraction: false}}
+    className="w-full h-48 xl:h-96 lg:h-64 bg-dyr "
     >
-</Swiper>
+
+
+    </Swiper>
     </div>
   )
 }
