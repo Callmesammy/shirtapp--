63043ae728c4f8cff80e364297375aedde271c7c
Callmesammy <callmesammy@yahoo.com>
2023-08-28 23:21:01 +0100

    a

--- src/Screens/Dashboards/Homescreen.js
+++ src/Screens/Dashboards/Homescreen.js
@@ -5,7 +5,7 @@ import Layout from '../../Layout/Layout'
 function Homescreen() {
   return (
    < Layout>
-   <div className="container mx-auto mb-6 min-h-screen">HomeScreen</div>
+   <div className="container mx-auto mb-6 min-h-screen px-2">HomeScreen</div>
    </Layout>
    
   );
