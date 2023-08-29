32c159c3cd3574a26a60a04dc0de5de64dcf2366
Callmesammy <callmesammy@yahoo.com>
2023-08-29 01:12:25 +0100

    ww

--- src/Home/Banner.js
+++ src/Home/Banner.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Movies } from '../Movies/Films';
-
+import { Movies } from '../Components/Film';
 
 
 function Banner() {
