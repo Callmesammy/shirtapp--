5bfe921d9b52991028eee99a01c4636d8a9f3ca0
Callmesammy <callmesammy@yahoo.com>
2023-08-31 00:04:11 +0100

    ss

--- src/Screens/Dashboards/SideBar.js
+++ src/Screens/Dashboards/SideBar.js
@@ -3,7 +3,9 @@ import { MdDashboardCustomize, MdMovieFilter } from 'react-icons/md'
 import { BiCameraHome, BiSolidCategoryAlt } from 'react-icons/bi'
 import { BsGridFill } from 'react-icons/bs'
 import { AiOutlineUser } from 'react-icons/ai'
-import Layout from '../../Components/Layout'
+import Layout from '../../Layout/Layout'
+
+
 
 
 function SideBar() {
@@ -39,16 +41,11 @@ function SideBar() {
             link: "/categories",
             icons:BiSolidCategoryAlt,
         },
-    ]
+    ];
   return (
     <Layout>
-    <div className="min-h-screen mx-auto container px-2">
-    <div className="gap-10 xl:grid grid-cols-8 items-start md-py-12 py-6">
-    <div className="col-span-2 sticky p-6 bg-dyr border border-gray-600 mb-5 xl:mb-0 rounded-md">
-    </div>
-    </div>
-    </div>  
-     </Layout>
+        
+    </Layout>  
   )
 }
 
