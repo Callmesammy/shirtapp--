ada8d4fcf9a9a73a42e4f0b44044345bf4fd4958
Callmesammy <callmesammy@yahoo.com>
2023-08-31 01:00:37 +0100

    dd

--- src/Screens/Dashboards/SideBar.js
+++ src/Screens/Dashboards/SideBar.js
@@ -4,11 +4,18 @@ import { BiCameraHome, BiSolidCategoryAlt } from 'react-icons/bi'
 import { BsGridFill } from 'react-icons/bs'
 import { AiOutlineUser } from 'react-icons/ai'
 import Layout from '../../Layout/Layout'
+import { NavLink } from 'react-router-dom'
 
 
 
 
 function SideBar() {
+    const active = "bg-dyd text-sub"
+    const hover =  "hover:bg-maint hover:text-sub"
+    const inActive = "rounded font-medium text-sm transitions px-4 flex gap-3 items-center p-4"
+    const Hover =({isActive}) =>
+    isActive ?  '{active} ${inActive}`' : '`${hover} ${inActive}`'
+    
 
     const linkList = [
         {
@@ -47,8 +54,15 @@ function SideBar() {
         < div className="container mx-auto min-h-screen px-2">
         <div className="xl:grid grid-cols-8  gap-10 items-start md:py-12 py-6">
         <div className="col-span-2 sticky bg-dyr border p-6  border-gray-800 rounded-md mb- xl:mb-0 ">
-        
-        </div>
+        {
+            //sidebar links
+         linkList.map((lin, items)=>(
+        <NavLink to={lin.link} key={items} className={Hover}>
+        <lin.icons /> <p>{lin.name}</p>
+        </NavLink>
+         ))
+        }
+        </div> 
         </div>
 
         </div>
